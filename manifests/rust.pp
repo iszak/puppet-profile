e2ebdb826e3fa1357d51b74b29a2691d90614e05
Iszak Bryan <iszak.bryan@gmail.com>
2015-08-21 19:20:02 +0100

    Added rust profile

---
+++ manifests/rust.pp
@@ -0,0 +1,13 @@
+class profile::rust (
+  ensure  => 'present',
+  version => 'stable',
+) {
+  validate_re($ensure, '^(present|absent)$')
+  validate_re($version, '^(stable|nightly|0\.[0-9]+)$')
+
+  class { '::rustlang':
+    ensure       => $ensure,
+    package_name => "${package_name}-${version}",
+  }
+}
+

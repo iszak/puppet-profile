60e0f1b507df85c11493ca482a352e0af824a72f
Iszak Bryan <iszak.bryan@gmail.com>
2015-08-21 23:25:35 +0100

    Lock django to 1.5

--- manifests/monitor/backend.pp
+++ manifests/monitor/backend.pp
@@ -1,3 +1,9 @@
 class profile::monitor::backend {
-  class { '::graphite': }
+  class { '::graphite':
+    gr_web_server      => 'apache',
+
+    gr_django_pkg      => 'django',
+    gr_django_ver      => '1.5',
+    gr_django_provider => 'pip',
+  }
 }
